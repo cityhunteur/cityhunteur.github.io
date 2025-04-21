b11095b6aefec40fd75c6ba484cc1d01b542ca2e
Pravin Goomannee <5994387+Cityhunteur@users.noreply.github.com>
2025-04-21 15:27:49 +0100

    chore: bump deps to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cityhunteur.github.io',
   outDir: './dist', // Output directory for the build
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
   build: {
     assets: 'assets',
+  },
+  vite: {
+    css: {
+      postcss: './postcss.config.cjs',
+    },
   },
 });
